dbd04bbb113418e1d0fb36c654a785a44728b6cb
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-05 19:48:28 +0700

    Up again

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -165,7 +165,7 @@ function ChatWindow({ user, chatType, targetId, targetName, targetUserId, target
           setMessages((prev) => [...prev, message]);
           console.log(data);
           // Mark as read เมื่อรับข้อความใหม่ (ถ้าหน้าต่างแชทนี้เปิดอยู่)
-          if (data.groupName === targetUserId) {
+          if (data.groupName === targetId) {
             socket.emit('mark_group_read', {
               userId: user.id,
               groupName: targetId
