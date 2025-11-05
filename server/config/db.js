0c7f9a66e5117df0d0e1dfd28a43a1f08da7c3fb
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-05 16:05:17 +0700

    split logic out of route file

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+export const connectDB = async()=>{
+    const mongo_URI = process.env.MONGODB_URI
+    if (!mongo_URI){
+        console.error("MONGO_URI is not set");
+        process.exit(1)
+    }
+    try{
+        await mongoose.connect(mongo_URI)
+        console.log("Successfuly connect MONGODB");
+    }catch(err){
+        console.error('MongoDB connection error:', err);
+        process.exit(1)
+    }
+}
